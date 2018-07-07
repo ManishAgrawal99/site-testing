3d19172d7cd426f368d672579537dedbbb088050
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-07-08 02:27:12 +0530

    GET Method Added

--- HOME PAGE/js/script.js
+++ HOME PAGE/js/script.js
@@ -9,46 +9,53 @@ window.onclick = function(event) {
     }
 }
 
-/**Service page dropdown**/
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-document.getElementById("homedrp").addEventListener("click", getHomeSelectedValue);
-document.getElementById("aboutdrp").addEventListener("click", getAboutSelectedValue); 
-document.getElementById("contactdrp").addEventListener("click", getContactSelectedValue);
-
-function getHomeSelectedValue() {
-    var selectedValue = "home";
-    console.log(selectedValue);
-    document.getElementById("house-type-selected").textContent=selectedValue;
-}
-function getAboutSelectedValue() {
-    var selectedValue = "About";
-    console.log(selectedValue);
-    document.getElementById("house-type-selected").textContent=selectedValue;
-}
-function getContactSelectedValue() {
-    var selectedValue = "contact";
-    console.log(selectedValue);
-    document.getElementById("house-type-selected").textContent=selectedValue;
-}
+document.getElementById("header-signup").addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = res.firstName + " " + res.lastName
+              console.log(message);
+              document.getElementById("header-signup").textContent=message;
+              document.getElementById("header-signup").onclick = function(){
+              window.location.href = "index.html";
 
 
+              };
+            });
+      });
+
+
+// //Posting Method 1
+// var arr = { City: 'Moscow', Age: 25 };
+// $.ajax({
+//     url: 'Ajax.ashx',
+//     type: 'POST',
+//     data: JSON.stringify(arr),
+//     contentType: 'application/json; charset=utf-8',
+//     dataType: 'json',
+//     async: false,
+//     success: function(msg) {
+//         alert(msg);
+//     }
+// });
+
+// //Posting Method 2
+// $.ajax({
+//     url: 'users.php',
+//     dataType: 'json',
+//     type: 'post',
+//     contentType: 'application/json',
+//     data: JSON.stringify( { "first-name": $('#first-name').val(), "last-name": $('#last-name').val() } ),
+//     processData: false,
+//     success: function( data, textStatus, jQxhr ){
+//         $('#response pre').html( JSON.stringify( data ) );
+//     },
+//     error: function( jqXhr, textStatus, errorThrown ){
+//         console.log( errorThrown );
+//     }
+// });
+
+// //Check this to have a look at post method
+// //https://api.jquery.com/jquery.post/
