c5cec585e8c7e50a80ff988c947063714e6d0ba2
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-07-09 17:52:18 +0530

    Final piece

--- HOME PAGE/js/empPortal.js
+++ HOME PAGE/js/empPortal.js
@@ -6,18 +6,41 @@ document.addEventListener("DOMContentLoaded",
                 }
             });
       $.ajax({
-                url : "data/personal.json",
+                url : "/api/emp",
                 dataType : "json",
                 type : "get",
                 success: function( data, textStatus, jQxhr ){
                   console.log("Inside get success");
                   var details = data;
                   console.log(details);
-                  var personalInfoString = "<table><tbody><tr><td class='portal-fields'>Name</td><td id=''>" + details.name + "</td></tr><tr><td class='portal-fields'>Supervisors Name</td><td id=''>" + details.supervisor + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.phone+ "</td></tr><tr><td class='portal-fields'>Employee ID</td><td id=''>" + details.eid + "</td></tr></tbody></table>";
-                  var currentJobString = "<table><tbody><tr><td class='portal-fields'>Customer Name</td><td id=''>" + details.custName + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.custPhone + "</td></tr><tr><td class='portal-fields'>Address</td><td id=''>" + details.custAddress+ "</td></tr><tr><td class='portal-fields'>Date & Time</td><td id=''>" + details.custDate + "</td></tr></tbody></table>";
+                  var personalInfoString = "<table><tbody><tr><td class='portal-fields'>Name</td><td id=''>" + details.EmployeeName + "</td></tr><tr><td class='portal-fields'>Supervisors Name</td><td id=''>" + details.SupervisorID + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.PhoneNo + "</td></tr><tr><td class='portal-fields'>Employee ID</td><td id=''>" + details._id + "</td></tr></tbody></table>";
+                  //var currentJobString = "<table><tbody><tr><td class='portal-fields'>Customer Name</td><td id=''>" + details.custName + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.custPhone + "</td></tr><tr><td class='portal-fields'>Address</td><td id=''>" + details.custAddress+ "</td></tr><tr><td class='portal-fields'>Date & Time</td><td id=''>" + details.custDate + "</td></tr></tbody></table>";
 
                   document.getElementById("myInfo")
                     .innerHTML = personalInfoString;
+
+                  //document.getElementById("current")
+                  //  .innerHTML = currentJobString;
+                      
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    alert(errorThrown );
+                }
+            });
+
+      $.ajax({
+                url : "/data/jobs",
+                dataType : "json",
+                type : "get",
+                success: function( data, textStatus, jQxhr ){
+                  console.log("Inside get success");
+                  var details = data;
+                  console.log(details);
+                  //var personalInfoString = "<table><tbody><tr><td class='portal-fields'>Name</td><td id=''>" + details.name + "</td></tr><tr><td class='portal-fields'>Supervisors Name</td><td id=''>" + details.supervisor + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.phone+ "</td></tr><tr><td class='portal-fields'>Employee ID</td><td id=''>" + details.eid + "</td></tr></tbody></table>";
+                  var currentJobString = "<table><tbody><tr><td class='portal-fields'>Customer Name</td><td id=''>" + details.custName + "</td></tr><tr><td class='portal-fields'>Phone Number</td><td id=''>" + details.custPhone + "</td></tr><tr><td class='portal-fields'>Address</td><td id=''>" + details.custAddress+ "</td></tr><tr><td class='portal-fields'>Date & Time</td><td id=''>" + details.custDate + "</td></tr></tbody></table>";
+
+                  //document.getElementById("myInfo")
+                  //  .innerHTML = personalInfoString;
 
                   document.getElementById("current")
                     .innerHTML = currentJobString;
@@ -26,5 +49,5 @@ document.addEventListener("DOMContentLoaded",
                 error: function( jqXhr, textStatus, errorThrown ){
                     alert(errorThrown );
                 }
-            })
+            });
   });
