632482e6531151206c32e1de66ac69dffb145fd2
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-07-09 12:58:18 +0530

    SignUp Login Tabs added

--- HOME PAGE/js/script.js
+++ HOME PAGE/js/script.js
@@ -9,6 +9,20 @@ window.onclick = function(event) {
     }
 }
 
+function openTabs(evt, buttnName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(buttnName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 /***Signup System***/
 
 
