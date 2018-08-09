6888303bc158e4b9ec49bebaefa8fe499b69494d
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-08-09 19:55:48 +0530

    Back end added

---
+++ iCleaners/OAuth/public/HOME PAGE/js/scriptCart.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded",
+  function (event){
+  	            $.ajaxSetup({
+                xhrFields: {
+                    withCredentials: true
+                }
+            });
+			$.ajax({
+                url : "data/name.json",
+                dataType : "json",
+                type : "get",
+                success: function( data, textStatus, jQxhr ){
+                	console.log("Inside get success");
+                	var cartHtmlString = "";
+                    var cartItems = data;
+                    console.log(cartItems.num);
+
+                    //We retrieve the num attribute from json
+                    for (var i = 0; i < cartItems.num ; i++) {
+                    	cartHtmlString = cartHtmlString + "<div><h4 id='cart-service-name'>Deep Cleaning</h4><h5>Our Service Includes</h5><ul><li>Bathroom, Kitchen, Living Room, Dining Room, Bedroom and Balcony Deep Cleaning</li><li>Dry vacuuming of sofa, curtain and carpets</li><li>Cleaning Time: 4 Hours, Cleaning Staff: 3</li><li>Item Total: Rs 1,000</li></ul></div>";
+                    }
+                    document.getElementById("cart-contents")
+                    .innerHTML = cartHtmlString;
+
+                    var totalPrice = cartItems.num * 1000;
+                    var n = totalPrice.toString();
+
+                    document.getElementById("cart-total-price")
+                    .innerHTML = "Total: Rs " + n;
+
+                    document.getElementById("summary-order-total")
+                    .innerHTML = "Order Total: Rs " + n;
+
+
+
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    alert(errorThrown );
+                }
+            })
+  });
---
+++ iCleaners/OAuth/public/HOME PAGE/js/scriptService.js
@@ -0,0 +1,42 @@
+
+/**Service page dropdown**/
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+document.getElementById("homedrp").addEventListener("click", getHomeSelectedValue);
+document.getElementById("aboutdrp").addEventListener("click", getAboutSelectedValue); 
+document.getElementById("contactdrp").addEventListener("click", getContactSelectedValue);
+
+function getHomeSelectedValue() {
+    var selectedValue = "home";
+    console.log(selectedValue);
+    document.getElementById("house-type-selected").textContent=selectedValue;
+}
+function getAboutSelectedValue() {
+    var selectedValue = "About";
+    console.log(selectedValue);
+    document.getElementById("house-type-selected").textContent=selectedValue;
+}
+function getContactSelectedValue() {
+    var selectedValue = "contact";
+    console.log(selectedValue);
+    document.getElementById("house-type-selected").textContent=selectedValue;
+}
